be3f062b05e07c879f5e359e13bcbc7793011b1b
Joaquin Barcena <joaquin.barcena@mercadolibre.com>
2022-05-02 14:35:13 -0300

    Update About Component

--- src/components/about.js
+++ src/components/about.js
@@ -1,42 +1,59 @@
 import './About.css'
 import 'terminal.css'
+import pokemon from '../assets/pokemon.mp3'
 import Typed from 'typed.js'
 import React from 'react'
 
 /**
  * First page to show with info aboutMe
  */
-export default function AboutMe() {
+export default function AboutMe(props) {
     const elem = React.useRef()
     const typed = React.useRef()
+    const pokeAudio = React.useRef()
 
     React.useEffect(() => {
         typed.current = new Typed(elem.current, {
             strings: [
-                "boring programmer",
-                "f*king weirdo",
-                "don't hire him"
+                "> just another dev",
+                "> fire-able",
+                "> don't hire him",
+                "> y're warned",
             ],
             typeSpeed: 50,
             backSpeed: 40,
             backDelay: 1500,
-            loop: true
+            loop: true,
+            smartBackspace: true,
         })
 
         return () => { typed.current.destroy() }
     }, [])
 
+    const playPokemon = () => {
+        pokeAudio.current.play()
+    }
+
     return (
         <div className="AboutContainer">
-            <div className='AboutCols' style={{ alignItems: "center" }}>
-                <div>Joaquin Barcena</div>
-                <div className='type-wrap' style={{ marginTop: "30px", color: "yellow" }}>
+            <div className='AboutCols' style={{ alignItems: 'flex-start'}}>
+                <span className='AboutName' >{ props.name }</span>
+                <div className='type-wrap AboutBanner'>
                     <span ref={elem}></span>
                 </div>
+                <div className='AboutSkillsContainer'>
+                { props.skills.map(s => 
+                    <span style={{ backgroundColor: s.backgroundColor, color: 'black', padding: '5px', margin: '5px'}}>
+                        { s.title }
+                    </span>
+                ) }
+                </div>
             </div>
-            <div className='AboutCols' style={{ alignItems: "center", }}>
-                <a type="button" class="btn btn-default " style={{ width: "70%" }}>Projects</a>
-                <a type="button" class="btn btn-default" style={{ width: "70%" }}>Reach Me !</a>
+            <div className='AboutCols'>
+                <a href='#' type="button" class="btn btn-error AboutButtons">Timeline</a>
+                <a href='#' type="button" class="btn btn-primary AboutButtons">Projects</a>
+                <a href='#contact' onClick={playPokemon} type="button" class="btn btn-default AboutButtons">Reach Me!</a>
+                <audio ref={pokeAudio} src={pokemon}/>
             </div>
         </div>
     )
