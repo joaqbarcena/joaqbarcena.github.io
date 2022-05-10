35a374d37d185f26e23bbef4780a13f3f7867d9f
Joaquin Barcena <joaquin.barcena@mercadolibre.com>
2022-05-10 01:40:57 -0300

    Linter fix

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -4,7 +4,7 @@ export default function Timeline({ events }) {
 
     let buildDescription = (val) => {
         if (typeof val === 'string') val = [val]
-        let boldp = (x) => x.split("**").map((s, i) => ((i % 2 == 1) ? (<b style={{ color: '#96CEB4' }}>{s}</b>) : s))
+        let boldp = (x) => x.split("**").map((s, i) => ((i % 2 === 1) ? (<b style={{ color: '#96CEB4' }}>{s}</b>) : s))
         let buildList = (val) => (<ul> { val.map(element => (<li> {Array.isArray(element) ? buildList(element) : boldp(element)} </li>)) } </ul> )
         return val.map(x => Array.isArray(x) ? buildList(x) : (<blockquote>{boldp(x)}</blockquote>))
     }
